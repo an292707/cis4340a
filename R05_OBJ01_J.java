641ac4cde793857c3b52ad8e2042929bdae7e394
an292707 <an292707@ucf.edu>
2025-02-24 08:30:28 -0500

    Update R05_OBJ01_J.java
    Compliant Solution

--- R05_OBJ01_J.java
+++ R05_OBJ01_J.java
@@ -1,5 +1,14 @@
 // Rule 05. Object Orientation (OBJ)
 // OBJ01-J. Limit accessibility of fields
 
-public static final String[] items = {/* ... */};
+private static final String[] items = {/* ... */};
 
+public static final String getItem(int index) {
+  return items[index];
+}
+
+public static final int getItemCount () {
+  return items.length;
+}
+
+// Compliant :)
