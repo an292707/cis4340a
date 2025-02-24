09c8769ebac8bf42b7c0b44f900ef897ea8374b7
an292707 <an292707@ucf.edu>
2025-02-24 08:36:33 -0500

    Update R07_ERR04_J.java
    Compliant

--- R07_ERR04_J.java
+++ R07_ERR04_J.java
@@ -7,7 +7,10 @@ class TryFinally {
       throw new IllegalStateException();
     } finally {
       System.out.println("logic done");
-      return true;
     }
+    // Any return statements must go here;
+    // applicable only when exception is thrown conditionally
   }
 }
+
+// Compliant :)
