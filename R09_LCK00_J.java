0c30fb2c51236614144dafd83b9d19bca1d68864
an292707 <an292707@ucf.edu>
2025-02-24 09:05:06 -0500

    Update R09_LCK00_J.java

--- R09_LCK00_J.java
+++ R09_LCK00_J.java
@@ -2,15 +2,13 @@
 // LCK00-J. Use private final lock objects to synchronize classes that may interact with untrusted code
 
 public class SomeObject {
-  //changeValue locks on the class object's monitor  
-  public static synchronized void changeValue() {
-    // ...
+  private static final Object lock = new Object(); 
+
+  public static void changeValue() {
+    synchronized (lock) { // Locks on the private Object
+      // ...
+    }
   }
 }
 
-// Untrusted code
-synchronized (SomeObject.class) {
-  while (true) {
-    Thread.sleep(Integer.MAX_VALUE); // Indefinitely delay someObject
-  }
-}
+// Compliant :)
